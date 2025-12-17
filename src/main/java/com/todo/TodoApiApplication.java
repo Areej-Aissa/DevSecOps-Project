68f5dd4b7df0c73f1fad16914cc50227bfa79814
Areej-Aissa <arij.aissa.it@episousse.com.tn>
2025-12-17 19:58:40 +0100

    Initial commit

--- src/main/java/com/todo/TodoApiApplication.java
+++ src/main/java/com/todo/TodoApiApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-private static final String GITHUB_TOKEN = "ghp_1234567890abcdef1234567890abcdef1234";
+//private static final String GITHUB_TOKEN = "ghp_1234567890abcdef1234567890abcdef1234";
 public class TodoApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(TodoApiApplication.class, args);
