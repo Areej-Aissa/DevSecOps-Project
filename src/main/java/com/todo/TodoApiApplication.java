bcab50fb62c3b5ed408117ea5d2dd072e9819ead
Areej-Aissa <arij.aissa.it@episousse.com.tn>
2025-12-17 20:12:36 +0100

    feat: add gitleaks security scan

--- src/main/java/com/todo/TodoApiApplication.java
+++ src/main/java/com/todo/TodoApiApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//private static final String GITHUB_TOKEN = "ghp_1234567890abcdef1234567890abcdef1234";
 public class TodoApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(TodoApiApplication.class, args);
